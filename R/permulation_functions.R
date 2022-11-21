18d7536745dc263af63191cdd4a98cf0a4c584c3
ECSaputra <elysia.cecilia@gmail.com>
2022-11-21 17:29:56 -0500

    updated permulation_functions.R

--- R/permulation_functions.R
+++ R/permulation_functions.R
@@ -4,18 +4,22 @@ require('RERconverge')
 #' @param foregrounds a vector of foreground species names
 #' @param neutraltree a phylo object representing neutral evolution
 #' @param root_species the species to root the tree on
-#' @param sisters_list a list object containing pair(s) of sister species whose common ancestor(s) should be included in the foreground set
 #' @return permulated_tree a permulated phenotype tree with binary edge lengths (foreground edges = 1, background edges = 0)
 #' @export
-getPermulatedPhenotypeTree=function(foregrounds, neutraltree, root_species, sisters_list=NULL){
+getPermulatedPhenotypeTree=function(foregrounds, neutraltree, root_species){
   foreground_tree = getTreeFromForegrounds(foregrounds, neutraltree, plotTree=F)
+  binary_permulation_inputs = getBinaryPermulationInputsFromTree(foreground_tree)
+  sisters_list = binary_permulation_inputs$sisters_list
+  fg_vec = binary_permulation_inputs$fg_vec
+
   pathvec = getPathvecfromForegroundTree(foreground_tree)
 
   masterTree = list()
   masterTree[[1]] = neutraltree
   names(masterTree) = c("masterTree")
 
-  permulated_tree = simBinPhenoSSM(foreground_tree, masterTree, root_species, foregrounds, sisters_list=sisters_list, pathvec, plotTreeBool = F)
+  permulated_tree = simBinPhenoCC(masterTree, neutraltree, root_species, fg_vec, sisters_list=sisters_list, pathvec, plotTreeBool = F)
+
   permulated_tree
 }
 
@@ -27,9 +31,9 @@ getPermulatedPhenotypeTree=function(foregrounds, neutraltree, root_species, sist
 #' @param output_mod flag for the format of the output ("names" outputs permulated foreground names, "tree" outputs permulated phenotype trees)
 #' @return a list object containing permulated phenotypes
 #' @export
-getPermulatedPhenotypes=function(foregrounds, neutraltree, num_perms, root_species, output_mod="names", sisters_list=NULL){
+getPermulatedPhenotypes=function(foregrounds, neutraltree, num_perms, root_species, output_mod="names"){
   fg_reps = lapply(1:num_perms, return_object, x=foregrounds)
-  permulated_trees = lapply(fg_reps, getPermulatedPhenotypeTree, neutraltree=neutraltree, root_species=root_species, sisters_list=sisters_list)
+  permulated_trees = lapply(fg_reps, getPermulatedPhenotypeTree, neutraltree=neutraltree, root_species=root_species)
 
   if (output_mod == "trees"){
     return(permulated_trees)
@@ -43,7 +47,8 @@ getPermulatedPhenotypes=function(foregrounds, neutraltree, num_perms, root_speci
 getPathvecfromForegroundTree=function(foreground_tree){
   fg_edge_idx = which(foreground_tree$edge.length==1)
   fg_node_idx = foreground_tree$edge[fg_edge_idx,2]
-  foreground_species = foreground_tree$tip.label[fg_node_idx]
+  idx_tip_fg = which(fg_node_idx <= length(foreground_tree$tip.label))
+  foreground_species = foreground_tree$tip.label[fg_node_idx[idx_tip_fg]]
 
   pathvec = rep(0, length(foreground_tree$tip.label))
   names(pathvec) = foreground_tree$tip.label
